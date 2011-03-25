99a2fa6ef6dc795624a719e0c9164fd59f7d6c89
boris <bnagaev@gmail.com>
2011-03-25 17:21:25 +0300

    Markup class added

---
+++ allpp/Markup.hpp
@@ -0,0 +1,21 @@
+#ifndef ALLPP_MARKUP_HPP_
+#define ALLPP_MARKUP_HPP_
+
+#include <vector>
+
+#include "allpp/MonomerType.hpp"
+#include "allpp/Monomer.hpp"
+
+namespace allpp {
+
+template <MonomerType mt, template<MonomerType> class Element>
+class Markup
+{
+public:
+private:
+};
+
+}
+
+#endif
+
