0f3fe99b08af18d6288efe928e16c6d2a3553a44
boris <bnagaev@gmail.com>
2011-03-25 23:01:11 +0300

    create BaseAlignment class, again
    move index() and move_monomer() function to BaseAlignment

--- allpp/Alignment.hpp
+++ allpp/Alignment.hpp
@@ -11,40 +11,46 @@
 namespace allpp {
 
 template <MonomerType mt>
+class BaseAlignment
+{
+public:
+    virtual void move_monomer(const Sequence* sequence,
+        Column<mt>* source, Column<mt>* dest, bool flush=false,
+        bool check=true) = 0;
+
+    virtual int index(const Column<mt>* column_) const = 0;
+
+private:
+    std::vector<Sequence<mt>* > sequences_;
+    std::vector<Column<mt>* > columns_;
+};
+
+template <MonomerType mt>
 class Block;
 
 template <MonomerType mt>
-class Alignment
+class Alignment : public BaseAlignment
 {
 public:
     ~Alignment();
 
     void detach_sequences();
 
-    int column_index(const Column<mt>* column_) const;
+    int index_in_alignment(const Column<mt>* column_) const;
 
     void insert_gap_column(Column<mt>* column); // before
     void drop_gap_column(Column<mt>* column);
 
-    void move_monomer(const Sequence* sequence,
-        Column<mt>* source, Column<mt>* dest, bool check=true);
-    void flush_monomers(const Sequence* sequence,
-        Column<mt>* source, Column<mt>* dest, bool check=true);
-
 private:
-    std::vector<Sequence<mt>* > sequences_;
-    std::vector<Column<mt>* > columns_;
     std::vector<Block<mt> > blocks_;
 };
 
 template <MonomerType mt>
-class Block
+class Block : public BaseAlignment
 {
 public:
 private:
     Alignment<mt>& alignment_;
-    std::vector<Sequence<mt>* > sequences_;
-    std::vector<Column<mt>* > columns_;
     std::vector<Markup<mt, Block> > markups_;
 };
 
